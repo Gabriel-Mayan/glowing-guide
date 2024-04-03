a87e9ab110a5555d4d8ce726db7bfabbc075e81c
Gabriel Mayan <gabrielmayan@gmail.com>
2024-04-03 10:59:17 -0300

    readme, ajustes no .env.exemple e no server

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import InitializeSentry from "@shared/services/sentry.service";
 import app from "./app";
 
 const port = process.env.PORT as string || "8080";
+const host = process.env.HOST as string || "http://localhost";
 
 if (process.env.SENTRY_URL) {
     InitializeSentry(process.env.SENTRY_URL);
@@ -13,5 +14,5 @@ if (process.env.SENTRY_URL) {
 
 AppDataSource.initialize().then(async () => {
     console.log("Connection initialized with database...");
-    app.listen(port, () => console.log(`Running on http://localhost:${port}`));
+    app.listen(port, () => console.log(`Running on ${host}:${port}`));
 }).catch((error: Error) => console.log(error));
